1facf1bcaf08587f4585f4da3fe505abcaf4f35d
Leedabord <gwforumlakers@gmail.com>
2020-06-25 23:01:41 -0700

    4 files modified, 7 added, 7 deleted

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,53 +1,34 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-
 import { MyApp } from './app.component';
 
-import { LearnFeedPage } from '../pages/learn-feed/learn-feed';
-import { LearnDetailsPage } from '../pages/learn-details/learn-details';
-import { QuestionDetailsPage } from '../pages/question-details/question-details';
-import { ManageQuestionPage } from '../pages/manage-question/manage-question';
-import { ManageAnswerPage } from '../pages/manage-answer/manage-answer';
-
-import { QuestionService } from '../services/question.service';
-import { AnswerService } from '../services/answer.service';
-import { LearnService } from '../services/learn.service';
-
-import { BrowserModule } from '@angular/platform-browser';
-
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { SDKBrowserModule } from '../sdk/index';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
   declarations: [
     MyApp,
-    LearnFeedPage,
-    LearnDetailsPage,
-    QuestionDetailsPage,
-    ManageQuestionPage,
-    ManageAnswerPage
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
-    SDKBrowserModule.forRoot()
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    LearnFeedPage,
-    LearnDetailsPage,
-    QuestionDetailsPage,
-    ManageQuestionPage,
-    ManageAnswerPage
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
-    QuestionService,
-    AnswerService,
-    LearnService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
